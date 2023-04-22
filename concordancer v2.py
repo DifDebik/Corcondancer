cd6bbfce60ed7517c51bed98e93cc850f319aef7
DifDeb <zzeni42@gmail.com>
2023-04-22 22:25:53 +0300

    Update concordancer v2.py

--- concordancer v2.py
+++ concordancer v2.py
@@ -1,3 +1,9 @@
+import spacy
+import pandas as pd
+from string import punctuation
+
+punc = set(punctuation)
+
 def find_concordance(text, lemmas):
     nlp = spacy.load('ru_core_news_lg')
     doc = nlp(text)
@@ -5,14 +11,17 @@ def find_concordance(text, lemmas):
 
     for token in doc:
         if token.lemma_.lower() in lemmas:
-            left_context = ' '.join([t.text for t in token.sent if t.i < token.i])
-            right_context = ' '.join([t.text for t in token.sent if t.i > token.i])
-            concordance.append([left_context, token.text, right_context])
+            left_context = [t.text for t in token.sent if t.i < token.i]
+            left_context2 = ''.join(w if set(w) <= punc else ' '+w for w in left_context).lstrip().replace('« ', '«').replace(' »', '»')
+            right_context = [t.text for t in token.sent if t.i > token.i]
+            right_context2 = ''.join(w if set(w) <= punc else ' '+w for w in right_context).lstrip().replace('« ', '«').replace(' »', '»')
+            
+            concordance.append([left_context2, token.text, right_context2])
     return concordance
 
 # Example usage
 text = "В лингвистике термин «текст» используется в широком значении, включая и образцы устной речи. Восприятие текста изучается в рамках лингвистики текста и психолингвистики. Так, например, И. Р. Гальперин определяет текст следующим образом: «Это письменное сообщение, объективированное в виде письменного документа, состоящее из ряда высказываний, объединённых разными типами лексической, грамматической и логической связи, имеющее определённый моральный характер, прагматическую установку и соответственно литературно обработанное»."
-lemmas = ["лингвистика", "значение"]
+lemmas = ["лингвистика", "значение", 'определять']
 
 concordance = find_concordance(text, lemmas)
 
