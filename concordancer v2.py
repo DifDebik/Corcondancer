eb321c23e0159c1875ed33f4e2b3a3a9878ffdf0
DifDeb <zzeni42@gmail.com>
2023-04-22 18:13:35 +0300

    Update concordancer v2.py

--- concordancer v2.py
+++ concordancer v2.py
@@ -1,13 +1,10 @@
-import spacy
-from tabulate import tabulate
-
-def find_concordance(text, lemma):
+def find_concordance(text, lemmas):
     nlp = spacy.load('ru_core_news_lg')
     doc = nlp(text)
     concordance = []
 
     for token in doc:
-        if token.lemma_.lower() == lemma.lower():
+        if token.lemma_.lower() in lemmas:
             left_context = ' '.join([t.text for t in token.sent if t.i < token.i])
             right_context = ' '.join([t.text for t in token.sent if t.i > token.i])
             concordance.append([left_context, token.text, right_context])
@@ -15,8 +12,14 @@ def find_concordance(text, lemma):
 
 # Example usage
 text = "В лингвистике термин «текст» используется в широком значении, включая и образцы устной речи. Восприятие текста изучается в рамках лингвистики текста и психолингвистики. Так, например, И. Р. Гальперин определяет текст следующим образом: «Это письменное сообщение, объективированное в виде письменного документа, состоящее из ряда высказываний, объединённых разными типами лексической, грамматической и логической связи, имеющее определённый моральный характер, прагматическую установку и соответственно литературно обработанное»."
-lemma = "образец"
+lemmas = ["лингвистика", "значение"]
 
-concordance = find_concordance(text, lemma)
-print("Concordance for lemma '{}':".format(lemma))
-print(tabulate(concordance, headers=["Left Context", "Instance", "Right Context"]))
+concordance = find_concordance(text, lemmas)
+
+# Convert concordance results to a DataFrame
+df = pd.DataFrame(concordance, columns=["Left Context", "Instance", "Right Context"])
+
+# Save DataFrame to Excel
+output_file = "concordance_results.xlsx"
+df.to_excel(output_file, index=False)
+print("Concordance results saved to '{}'".format(output_file))
